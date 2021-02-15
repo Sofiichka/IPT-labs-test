a4349b4075579f02c20fb8b9f1d4df189648b523
baltazar1697 <dimalgpf@ukr.net>
2021-02-15 16:20:51 +0200

    added tests for Hanoi

--- ASD/ASD_lab_Hanoi.py
+++ ASD/ASD_lab_Hanoi.py
@@ -1,9 +1,5 @@
 #description
 #by Baltazar1697
-height = int(input("Enter height "))#max disks
-fromPole = str(input("Enter starting pole "))
-toPole = str(input("Enter ending pole "))
-withPole = str(input("Enter temporary pole "))
 counter = 0
 def moveTower(height,fromPole, toPole, withPole):
     global counter
@@ -13,4 +9,3 @@ def moveTower(height,fromPole, toPole, withPole):
         counter+=1
         moveTower(height-1,withPole,toPole,fromPole)
     return counter
-print(moveTower(height,fromPole, toPole, withPole)) 
---
+++ test_.py
@@ -0,0 +1,9 @@
+import pytest
+from ASD.ASD_lab_Hanoi import moveTower
+
+fromPole = str("a")
+toPole = str("c")
+withPole = str("b")
+@pytest.mark.parametrize("height, expected",[(2,3),(3,7),(5,31),(10,1023)])
+def test_hanoi(height,expected):
+    assert moveTower(height,fromPole,toPole,withPole) == expected
