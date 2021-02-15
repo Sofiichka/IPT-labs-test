1f4d53240766de44c9a0b95cbb500996ca38a5b9
baltazar1697 <dimalgpf@ukr.net>
2021-02-15 17:40:42 +0200

    added sum to Fibonacci

--- ASD/ASD_lab_Fib.py
+++ ASD/ASD_lab_Fib.py
@@ -1,11 +1,14 @@
 #description
 #by Baltazar1697
 a = int(input('Input which number of Fibonacci you want to know '))
+sum = 0
 def Fib(a):
+    global sum
     if a == 0:
         return 1
     elif a == 1:
         return 1
     else:
-        return Fib(a-2)+Fib(a-1)
-print(Fib(a))
+        sum = Fib(a-2)+Fib(a-1)
+        return sum
+print(Fib(a)-1)
