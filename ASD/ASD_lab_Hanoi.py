c63782fbb33886032addd0edbbe78db067589287
baltazar1697 <dimalgpf@ukr.net>
2021-02-15 13:35:32 +0200

    fixed hanoi:latest

--- ASD/ASD_lab_Hanoi.py
+++ ASD/ASD_lab_Hanoi.py
@@ -1,18 +1,16 @@
 #description
 #by Baltazar1697
 height = int(input("Enter height "))#max disks
-fromPole = str(input("Enter starting pole"))# number
-toPole = str(input("Enter ending pole"))
-withPole = str(input("Enter temporary pole"))
+fromPole = str(input("Enter starting pole "))
+toPole = str(input("Enter ending pole "))
+withPole = str(input("Enter temporary pole "))
 counter = 0
 def moveTower(height,fromPole, toPole, withPole):
+    global counter
     if height >= 1:
         moveTower(height-1,fromPole,withPole,toPole)
-        moveDisk(fromPole,toPole)
+        print("moving disk from",fromPole,"to",toPole)
+        counter+=1
         moveTower(height-1,withPole,toPole,fromPole)
-
-def moveDisk(fp,tp):
-    print("moving disk from",fp,"to",tp)
-moveTower(height,fromPole, toPole, withPole)
-# к-во ходов 2^n-1
-print(counter)
+    return counter
+print(moveTower(height,fromPole, toPole, withPole)) 
