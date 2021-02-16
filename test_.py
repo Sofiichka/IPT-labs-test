86758797e03ea98a6234656307439618ab5306ae
baltazar1697 <dimalgpf@ukr.net>
2021-02-16 14:46:11 +0200

    meow

--- test_.py
+++ test_.py
@@ -5,7 +5,7 @@ from ASD.ASD_lab_Fib import Fib
 fromPole = str("a")
 toPole = str("c")
 withPole = str("b")
-@pytest.mark.parametrize("height, expected",[(2,3),(3,7),(5,31),(10,1023)])
+@pytest.mark.parametrize("height, expected",[(2,3),(3,7),(5,31),(10,1023),(4,15)])
 def test_hanoi(height,expected):
     assert moveTower(height,fromPole,toPole,withPole) == expected
 @pytest.mark.parametrize("input, exptected",[(2,2),(3,3),(5,8),(10,89)])
