cc3cf72ac4a0d3868288f677821e7c12914746c0
baltazar1697 <dimalgpf@ukr.net>
2021-02-16 13:59:39 +0200

    added FIb test

--- ASD/ASD_lab_Fib.py
+++ ASD/ASD_lab_Fib.py
@@ -1,6 +1,5 @@
 #description
 #by Baltazar1697
-a = int(input('Input which number of Fibonacci you want to know '))
 def Fib(a):
     if a == 0:
         return 1
@@ -8,4 +7,3 @@ def Fib(a):
         return 1
     else:
         return Fib(a-2)+Fib(a-1)
-print(Fib(a))
--- test_.py
+++ test_.py
@@ -1,5 +1,6 @@
 import pytest
 from ASD.ASD_lab_Hanoi import moveTower
+from ASD.ASD_lab_Fib import Fib
 
 fromPole = str("a")
 toPole = str("c")
@@ -7,3 +8,6 @@ withPole = str("b")
 @pytest.mark.parametrize("height, expected",[(2,3),(3,7),(5,31),(10,1023)])
 def test_hanoi(height,expected):
     assert moveTower(height,fromPole,toPole,withPole) == expected
+@pytest.mark.parametrize("input, exptected",[()])
+def test_fib(input, expected):
+    assert Fib(input) == expected
