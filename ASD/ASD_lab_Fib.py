95194c131a544c1110404982c26afcf482d36567
baltazar1697 <dimalgpf@ukr.net>
2021-02-16 14:04:59 +0200

    fixed tests

--- ASD/ASD_lab_Fib.py
+++ ASD/ASD_lab_Fib.py
@@ -1,9 +1,12 @@
 #description
 #by Baltazar1697
+sum = 0
 def Fib(a):
+    global sum
     if a == 0:
         return 1
     elif a == 1:
         return 1
     else:
-        return Fib(a-2)+Fib(a-1)
+        sum = Fib(a-2)+Fib(a-1)
+        return sum
--- test_.py
+++ test_.py
@@ -8,6 +8,6 @@ withPole = str("b")
 @pytest.mark.parametrize("height, expected",[(2,3),(3,7),(5,31),(10,1023)])
 def test_hanoi(height,expected):
     assert moveTower(height,fromPole,toPole,withPole) == expected
-@pytest.mark.parametrize("input, exptected",[()])
-def test_fib(input, expected):
-    assert Fib(input) == expected
+@pytest.mark.parametrize("input, exptected",[(2,2),(3,3),(5,8),(10,89)])
+def test_fib(input, exptected):
+    assert Fib(input) == exptected
