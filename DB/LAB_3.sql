07dfe5769592481a747ba7d0324fbcc412473304
Dima <dimalgpf@ukr.net>
2021-10-24 22:53:02 +0300

    Fixed labs 3

--- DB/LAB_3.sql
+++ DB/LAB_3.sql
@@ -2,16 +2,28 @@ USE [Commercial]
 GO
 
 INSERT INTO Specializations(Name, Cost) VALUES
-('AppliedMaths',22500), ('Cybersecurity',31000),('AppliedPhysics', 16800);
+('AppliedMaths',22500),
+('Cybersecurity',31000),
+('AppliedPhysics', 16800),
+('Linguistics(English)', 18500),
+('Linguistics(German)', 18500),
+('Linguistics(French)', 18500),
+('Journalistics', 37400),
+('Physics', 28700),
+('Arts', 42100),
+('Economics', 37300);
 
 INSERT INTO Student_Groups(Name, Specialization_FK, Course, Term) VALUES
 ('FI-92',1,3,1),
 ('FI-94',1,3,1),
-('FI-02',1,2,1),
+('FI-02',10,2,1),
 ('FB-92',2,3,1),
 ('FF-11',3,1,1),
-('FF-01',3,2,1),
-('FI-12',1,1,1)
+('FF-01',9,2,1),
+('FI-93',1,3,1),
+('FB-12',4,2,1),
+('FA-12',8,1,1),
+('FM-12',7,2,1);
 
 INSERT INTO Students(Name, Surname, Sex, Group_FK, Birth, Billed_for_studying) VALUES
 ('Dmytro','Romanchenko','M', 1, '2002-07-19', 1),
@@ -19,4 +31,8 @@ INSERT INTO Students(Name, Surname, Sex, Group_FK, Birth, Billed_for_studying) V
 ('Egor', 'Panasuk', 'M', 2, '2001-05-22', 1),
 ('Anton', 'Yanushev', 'M', 1, '2002-08-16', 0),
 ('Dmytro', 'Zibarof', 'M', 4, '2002-01-01', 1),
-('Maria', 'Cherednychenko', 'W', 3, '2003-10-12', 1)
+('Ivan', 'Ponomarenko', 'M', 3, '2003-10-12', 1),
+('Nikita', 'Shashenok', 'M', 6, '2001-11-21', 1),
+('Alex', 'Oliynuk', 'M', 4, '2002-05-04', 0),
+('Vlad', 'Vasiliev', 'M', 7, '2003-09-07', 1),
+('Arsen', 'Alexin', 'M', 5, '2003-02-18', 0);
--- DB/Sofia_labs/Lab3_S.sql
+++ DB/Sofia_labs/Lab3_S.sql
@@ -6,11 +6,22 @@ INSERT INTO Units(Name, Unit_type) VALUES
 ('MMDA','Department'),
 ('AFNM', 'Department'),
 ('STIS','Department'),
+('FL', 'Facultie'),
+('IPT', 'Facultie'),
+('IASA', 'Institue'),
+('Security', 'Security'),
+('Library', 'Library'),
 ('Library', 'Library');
+
 
 INSERT INTO Rooms(type, Area, Unit) VALUES
 ('Library',22,5),
 ('Lab',31,1),
 ('Audience',43,1),
-('Lab',24,3),
-('Audience',42,2);
+('Lab',24,4),
+('Audience',55,6),
+('Lab',41,7),
+('Lab',29,10),
+('Lab',31,9),
+('Security',8,8),
+('Audience',70,3);
